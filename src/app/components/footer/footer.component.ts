a5837775ec2b10fb87c7260265667243cf4eb58e
Mauricio Godoy <100785540+mgodoyst@users.noreply.github.com>
2025-05-27 16:08:46 -0300

    feat: footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { LucideAngularModule, Linkedin, Github, MessageCircle, Mail } from 'lucide-angular';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
+  Linkedin = Linkedin;
+  Github = Github;
+  MessageCircle = MessageCircle;
+  Mail = Mail;
 
+  contactUrl = 'mailto:mauu.godoy01@gmail.com';
+  portfolioUrl = 'https://mauriciogodoy.vercel.app/';
+  projectsUrl = 'https://www.linkedin.com/in/mauriciongodoy/details/projects/';
+  linkedinUrl = 'https://www.linkedin.com/in/mauriciongodoy/';
+  githubUrl = 'https://github.com/Mauri-Godoy';
+  whatsappUrl = 'https://wa.me/5492664950406';
 }
