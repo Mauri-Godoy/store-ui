cc86b98c69dc351dea7cc7ccc223959d347eb24f
Mauricio Godoy <100785540+mgodoyst@users.noreply.github.com>
2025-05-27 15:52:04 -0300

    feat: visualizar carrito

--- src/app/pages/carts/carts.component.ts
+++ src/app/pages/carts/carts.component.ts
@@ -3,15 +3,17 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Cart } from '../../models/cart.model';
 import { CartService } from '../../services/cart.service';
+import { LucideAngularModule, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-carts',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LucideAngularModule],
   templateUrl: './carts.component.html'
 })
 export class CartsComponent {
-
+  X = X;
   cartsMap: { date: string; carts: Cart[] }[] = [];
+  cartSelected: Cart | null = null;
 
   constructor(private cartService: CartService) { }
 
@@ -28,5 +30,9 @@ export class CartsComponent {
         console.error(error);
       }
     });
+  }
+
+  selectCart(cart: Cart | null) {
+    this.cartSelected = cart;
   }
 }
